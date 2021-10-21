f64794e6dc9a080cd1f7a12ff20531593c72913e
Ida Norderhaug Drøsdal <ida.norderhaug.drosdal@ngi.no>
2021-10-21 19:15:33 +0200

    Demo replacing dependency

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
-# from matplotlib import pyplot as plt
+# import plotly.express as px
+import plotly.graph_objects as go
 
 
 def get_cat_fact():
@@ -10,24 +11,24 @@ def get_cat_fact():
         raise Exception(f"{res.status_code}: {res.reason}")
 
 
-# def plot_cat(color):
-#     with plt.xkcd():
-#         plt.plot([0, 1], [0, 0], color)
-#         plt.plot([0, 1], [1, 1], color)
-#         plt.plot([0, 0], [0, 1], color)
-#         plt.plot([1, 1], [0, 1], color)
-#
-#         for ear in [0.2, 0.8]:
-#             plt.plot([ear - 0.1, ear, ear + 0.1], [1, 1.2, 1], color)
-#         for whisker in [[0.3, 0.45], [0.5, 0.5], [0.7, 0.55]]:
-#             plt.plot([0.2, 0.4], whisker, color)
-#         for whisker in [[0.45, 0.3], [0.5, 0.5], [0.55, 0.7]]:
-#             plt.plot([0.6, 0.8], whisker, color)
-#
-#         plt.axis('off')
-#         plt.show()
+def plot_cat(color):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=[0, 1], y=[0, 0], mode='lines', line={"color": color}))
+    fig.add_trace(go.Scatter(x=[0, 1], y=[1, 1], mode='lines', line={"color": color}))
+    fig.add_trace(go.Scatter(x=[0, 0], y=[0, 1], mode='lines', line={"color": color}))
+    fig.add_trace(go.Scatter(x=[1, 1], y=[0, 1], mode='lines', line={"color": color}))
+
+    for ear in [0.2, 0.8]:
+        fig.add_trace(go.Scatter(x=[ear - 0.1, ear, ear + 0.1], y=[1, 1.2, 1], mode='lines', line={"color": color}))
+    for whisker in [[0.3, 0.45], [0.5, 0.5], [0.7, 0.55]]:
+        fig.add_trace(go.Scatter(x=[0.2, 0.4], y=whisker, mode='lines', line={"color": color}))
+    for whisker in [[0.45, 0.3], [0.5, 0.5], [0.55, 0.7]]:
+        fig.add_trace(go.Scatter(x=[0.6, 0.8], y=whisker, mode='lines', line={"color": color}))
+
+    fig.update_layout(showlegend=False)
+    fig.show()
 
 
 if __name__ == "__main__":
     print(get_cat_fact())
-    # plot_cat('k')
+    plot_cat('black')
